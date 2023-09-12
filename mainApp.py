eaeb229a8315210cff79fc264e0ffb81a7a7290f
hesam21188 <hosseinharatii@gmail.com>
2023-09-12 18:00:46 +0330

    finished widgets

--- mainApp.py
+++ mainApp.py
@@ -4,6 +4,7 @@ from tkinter import filedialog
 
 
 def get_cookie_file():
+    global cookie_file
     cookie_file = filedialog.askopenfilename(initialdir="/",
                                              title="Select your cookie File",
                                              filetypes=(("Text files",
@@ -12,25 +13,73 @@ def get_cookie_file():
                                                          "*.*")))
 
 
+def show_store_url_file():
+    store_url_file.place(relx=0.5, rely=0.5, y=50, x=0, anchor=CENTER)
+
+
+def select_store_url_file():
+    global store_urls_file
+    store_urls_file = filedialog.askopenfilename(initialdir="/",
+                                                 title="Select your store_url File",
+                                                 filetypes=(("Text files",
+                                                             "*.txt*"),
+                                                            ("all files",
+                                                             "*.*")))
+
+
+def start_download():
+    global low_quality, mp3, store_urls, no_download, is_course_range, cookie_file, store_urls_file, url
+    low_quality, mp3, store_urls, no_download, is_course_range = low_quality.get(), mp3.get(), store_urls.get(), \
+        no_download.get(), is_course_range.get()
+    url = str(inp_url.get())
+    range_start, range_end = str(inp_range_start.get()), str(inp_range_end.get())
+    print(low_quality, mp3, store_urls, no_download, is_course_range, cookie_file, store_urls_file, url)
+
+    dl.main(cookie_file, url, low_quality, mp3, store_urls, store_urls_file, no_download, is_course_range, range_start,
+            range_end)
+
 window = Tk()
 window.title("maktabkhooneh-dl")
 window.iconbitmap("")
-low_quality = None
-mp3 = None
-store_urls = None
-no_download = None
+
+low_quality = BooleanVar()
+mp3 = BooleanVar()
+store_urls = BooleanVar()
+no_download = BooleanVar()
+is_course_range = BooleanVar()
+cookie_file = StringVar()
+store_urls_file = StringVar()
+url = StringVar()
 
 button_explore = Button(window, text="choose cookie File", command=get_cookie_file)
 checkbutton_low_quality = Checkbutton(window, text='low quality', variable=low_quality, onvalue=True, offvalue=False)
 checkbutton_mp3 = Checkbutton(window, text='mp3', variable=mp3, onvalue=True, offvalue=False)
-checkbutton_store_urls = Checkbutton(window, text='store urls', variable=store_urls, onvalue=True, offvalue=False, )
+checkbutton_store_urls = Checkbutton(window, text='store urls', variable=store_urls, onvalue=True, offvalue=False,
+                                     command=show_store_url_file)
 checkbutton_no_download = Checkbutton(window, text='no download', variable=no_download, onvalue=True, offvalue=False)
+inp_url = Entry(window, font="arial 18")
+inp_text = Label(window, text="course url : ", font="arial 16")
+store_url_file = Button(window, text="choose store url file", command=select_store_url_file)
+checkbutton_range = Checkbutton(window, text='range ?', variable=is_course_range, onvalue=True, offvalue=False)
+inp_range_start = Entry(window, font="arial 18")
+inp_start_text = Label(window, text="range start : ", font="arial 16")
+inp_range_end = Entry(window, font="arial 18")
+inp_end_text = Label(window, text="range end : ", font="arial 16")
+start_download_btn = Button(window, text="start Download", command=start_download)
 
 button_explore.place(relx=0.5, rely=0.5, y=-100, x=0, anchor=CENTER)
 checkbutton_low_quality.place(relx=0.5, rely=0.5, y=-100, x=110, anchor=CENTER)
 checkbutton_mp3.place(relx=0.5, rely=0.5, y=-100, x=200, anchor=CENTER)
 checkbutton_store_urls.place(relx=0.5, rely=0.5, y=-100, x=-110, anchor=CENTER)
 checkbutton_no_download.place(relx=0.5, rely=0.5, y=-100, x=-200, anchor=CENTER)
+inp_text.place(relx=0.5, rely=0.5, y=-200, x=-200, anchor=CENTER)
+inp_url.place(relx=0.5, rely=0.5, y=-200, x=0, anchor=CENTER)
+checkbutton_range.place(relx=0.5, rely=0.5, y=50, x=0, anchor=CENTER)
+inp_range_start.place(relx=0.5, rely=0.5, y=100, x=0, anchor=CENTER)
+inp_start_text.place(relx=0.5, rely=0.5, y=100, x=-200, anchor=CENTER)
+inp_range_end.place(relx=0.5, rely=0.5, y=150, x=0, anchor=CENTER)
+inp_end_text.place(relx=0.5, rely=0.5, y=150, x=-200, anchor=CENTER)
+start_download_btn.place(relx=0.5, rely=0.5, y=0, x=0, anchor=CENTER)
 
 window.geometry(f"{int(window.winfo_screenwidth() * 0.75)}x{int(window.winfo_screenheight() * 0.75)}")
 window.mainloop()
--- maktabkhooneh_dl.py
+++ maktabkhooneh_dl.py
@@ -15,18 +15,19 @@ import requests
 from tqdm import tqdm
 
 
-def main():
+def main(cookie_file, url, low_quality, mp3, store_urls, store_urls_file, no_download, is_course_range, range_start,
+         range_end):
     config_logging()
-    args = parse_args()
+    # args = parse_args()
 
     # Load cookies
     logging.info("Loading cookies...")
-    cookies = MozillaCookieJar(args.cookies_file)
+    cookies = MozillaCookieJar(cookie_file)
     cookies.load()
 
     # Load course page
     logging.info("Loading course page...")
-    course_page = requests.get(args.course_url, cookies=cookies)
+    course_page = requests.get(url, cookies=cookies)
 
     # Parse course page and find links to lecture pages
     logging.info("Scraping lecture pages...")
@@ -47,25 +48,25 @@ def main():
     logging.info(f"Found {number_of_lectures} lectures.")
 
     # Check download range
-    if args.range:
-        start, end = args.range.split(':')
+    if is_course_range:
+        start, end = range_start, range_end
         if start == '':
             start = 1
         else:
-            start = int(start) - 1 # Because we start from zero
+            start = int(start) - 1  # Because we start from zero
         if end == '':
             end = number_of_lectures
         else:
-            end = int(end) # We still start from zero but our ending is exclusive
+            end = int(end)  # We still start from zero but our ending is exclusive
         download_range = range(int(start), int(end))
     else:
-        download_range = range(number_of_lectures) # Download all
+        download_range = range(number_of_lectures)  # Download all
     folder_index = 0
     last_folder = ''
 
     # open a file for storing download links if necessary 
-    if args.store_urls:
-        urls_file = open(args.store_urls, 'w')
+    if store_urls:
+        urls_file = open(store_urls_file, 'w')
 
     # Download lectures
     for i in download_range:
@@ -75,18 +76,18 @@ def main():
             continue
 
         title = sanitize_for_filename(title)
-        final_extension = "mp3" if args.mp3 else "mp4"
+        final_extension = "mp3" if mp3 else "mp4"
         intermediate_file = f"{title}.mp4.incomplete"
-        final_file = f"{(i+1):03d}-{title}.{final_extension}"
+        final_file = f"{(i + 1):03d}-{title}.{final_extension}"
 
         # Bail if lecture is already downloaded
         if os.path.exists(final_file):
-            logging.info(f"Lecture {i+1} exists. Skipping...")
+            logging.info(f"Lecture {i + 1} exists. Skipping...")
             continue
 
         # Fetch lecture page
         print()
-        logging.info(f"Loading lecture page ({i+1}/{number_of_lectures})...")
+        logging.info(f"Loading lecture page ({i + 1}/{number_of_lectures})...")
         lecture_page = requests.get(make_absolute(url), cookies=cookies)
 
         # Find download links
@@ -95,9 +96,9 @@ def main():
         section = bs.select('.breadcrumb__item a')
 
         # specifying folder
-        folder = '' # no special folder
+        folder = ''  # no special folder
         if len(section) > 1:
-            folder = section[len(section)-2].get_text(strip=True)
+            folder = section[len(section) - 2].get_text(strip=True)
         elif len(section) > 0:
             folder = section[0].get_text(strip=True)
 
@@ -112,25 +113,25 @@ def main():
             final_file = numbered_folder + os.sep + final_file
 
         if os.path.exists(final_file):
-            logging.info(f"Lecture {i+1} exists. Skipping...")
+            logging.info(f"Lecture {i + 1} exists. Skipping...")
             continue
 
         all_download_links = bs.select('.unit-content--download a')
         if len(all_download_links) > 0:
-            HQ_download_link = all_download_links[0].get('href') # HQ comes first in page layout
+            HQ_download_link = all_download_links[0].get('href')  # HQ comes first in page layout
             if len(all_download_links) > 1:
-                LQ_download_link = all_download_links[1].get('href') # LG comes second in page layout
-                download_link = LQ_download_link if (args.low_quality or args.mp3) else HQ_download_link
+                LQ_download_link = all_download_links[1].get('href')  # LG comes second in page layout
+                download_link = LQ_download_link if (low_quality or mp3) else HQ_download_link
             else:
                 download_link = HQ_download_link
         else:
             # lets find the download urls from js video player ;)
             all_download_links = bs.select('.js-player__source')
             if len(all_download_links) > 0:
-                HQ_download_link = all_download_links[0].get('src') # HQ comes first in page layout
+                HQ_download_link = all_download_links[0].get('src')  # HQ comes first in page layout
                 if len(all_download_links) > 1:
-                    LQ_download_link = all_download_links[1].get('src') # LG comes second in page layout
-                    download_link = LQ_download_link if (args.low_quality or args.mp3) else HQ_download_link
+                    LQ_download_link = all_download_links[1].get('src')  # LG comes second in page layout
+                    download_link = LQ_download_link if (low_quality or mp3) else HQ_download_link
                 else:
                     download_link = HQ_download_link
             else:
@@ -138,50 +139,53 @@ def main():
                 logging.info('Maybe the page is a quize')
                 continue
 
-        if args.store_urls:
-            urls_file.write(make_absolute(download_link)+"\n")
+        if store_urls:
+            urls_file.write(make_absolute(download_link) + "\n")
 
         # Download
-        if not args.no_download:
+        if not no_download:
             logging.info("Downloading lecture file...")
             download(make_absolute(download_link), intermediate_file)
 
             # Convert/rename
-            if not args.mp3:
+            if not mp3:
                 # No further processing needed -- just rename
                 os.rename(intermediate_file, final_file)
             else:
                 # Convert
                 logging.info("Converting to mp3...")
-                subprocess.check_output(["ffmpeg", "-i", intermediate_file, final_file]) # TODO: Make non-blocking
+                subprocess.check_output(["ffmpeg", "-i", intermediate_file, final_file])  # TODO: Make non-blocking
                 os.remove(intermediate_file)
-        
+
         logging.info("Finished lecture.")
 
-    if args.store_urls:
+    if store_urls:
         urls_file.close()
     logging.info("Finished all.")
 
 
-def parse_args():
-    argument_parser = argparse.ArgumentParser(
-        prog="maktabkhooneh-dl",
-        description="This is a batch download utility for maktabkhooneh.org",
-        epilog=\
-            "Don't be cruel to their servers!\n"
-            "Only download what you really want to watch.\n"
-            "\n"
-            "Example: maktabkhooneh-dl cookies.txt https://maktabkhooneh.org/course/foo/\n",
-        formatter_class=argparse.RawTextHelpFormatter
-    )
-    argument_parser.add_argument("-L", "--low-quality", action="store_true")
-    argument_parser.add_argument("--mp3", action="store_true")
-    argument_parser.add_argument("--range", default=None, help="Only download a subset. Specify as `start:end` (inclusive) e.g. `--range=1:5`")
-    argument_parser.add_argument("cookies_file")
-    argument_parser.add_argument("course_url")
-    argument_parser.add_argument("--store-urls",help="Store download links in a file e.g. `--store-urls ./urls.txt --no-download`")
-    argument_parser.add_argument("--no-download", action="store_true", help="Don't download anything, useful when you only want to store download links")
-    return argument_parser.parse_args()
+# def parse_args():
+#     argument_parser = argparse.ArgumentParser(
+#         prog="maktabkhooneh-dl",
+#         description="This is a batch download utility for maktabkhooneh.org",
+#         epilog= \
+#             "Don't be cruel to their servers!\n"
+#             "Only download what you really want to watch.\n"
+#             "\n"
+#             "Example: maktabkhooneh-dl cookies.txt https://maktabkhooneh.org/course/foo/\n",
+#         formatter_class=argparse.RawTextHelpFormatter
+#     )
+#     argument_parser.add_argument("-L", "--low-quality", action="store_true")
+#     argument_parser.add_argument("--mp3", action="store_true")
+#     argument_parser.add_argument("--range", default=None,
+#                                  help="Only download a subset. Specify as `start:end` (inclusive) e.g. `--range=1:5`")
+#     argument_parser.add_argument("cookies_file")
+#     argument_parser.add_argument("course_url")
+#     argument_parser.add_argument("--store-urls",
+#                                  help="Store download links in a file e.g. `--store-urls ./urls.txt --no-download`")
+#     argument_parser.add_argument("--no-download", action="store_true",
+#                                  help="Don't download anything, useful when you only want to store download links")
+#     return argument_parser.parse_args()
 
 
 def config_logging():
@@ -208,11 +212,11 @@ def download(url: str, fname: str):
         resp = requests.get(url, stream=True)
         total = int(resp.headers.get('content-length', 0))
         with open(fname, 'wb') as file, tqdm(
-            desc="        Progress",
-            total=total,
-            unit='iB',
-            unit_scale=True,
-            unit_divisor=1024,
+                desc="        Progress",
+                total=total,
+                unit='iB',
+                unit_scale=True,
+                unit_divisor=1024,
         ) as bar:
             for data in resp.iter_content(chunk_size=1024):
                 size = file.write(data)
@@ -223,7 +227,6 @@ def download(url: str, fname: str):
         else:
             os.remove(fname)
             logging.warning("downloaded file corrupted. retrying...")
-    
 
 
 def exception_handler(_type, value, _tb):
